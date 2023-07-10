887ca35274c726dcdd1ea286651354afced72912
MaksimNifedov <105967081+MaksimNifedov@users.noreply.github.com>
2023-07-10 13:56:28 +0800

    Новая демо-версия
    Новая демо-версия

--- local/php_interface/constID.php
+++ local/php_interface/constID.php
@@ -4,21 +4,20 @@
  */
 define('IDSocialsHL', '2'); // ID HL-блока, отвечающий за ссылки на соц. сети в подвале сайта
 define('IDContacsHL', '3'); // ID HL-блока для контактов заказчика (находится левее от веб-формы)
-
 /*
  * Константы для обозначения ID ИБ
  */
-define('IDVideoIB', '17'); // ID ИБ для видео
-define('IDPhotoIB', '13'); // ID ИБ для фото
-define('IDQuotesIB', '16'); // ID ИБ для блока цитат
-define('IDPartnersIB', '21'); // ID ИБ для блока с партнерами
-define('IDNewsIB', '14'); // ID ИБ для блока с новостями
-define('IDNominationIB', '15'); // ID ИБ для блока с описанием номинаций
-define('IDJuryIB', '12'); // ID ИБ для блока с информацией о жюри
-define('IDValuablePlayerIB', '11'); // ID ИБ для блока ценные игроки
-define('IDGentlemanYearIB', '19'); // ID ИБ для блока с лауреатами премии джентельмен года
-define('IDBestNewcomerIB', '20'); // ID ИБ для блока с лауреатами премии лучший новичок
-define('IDMostValuablePlayerIB', '18'); // ID ИБ для блока с лауреатами премии самый лучший игрок
+define('IDVideoIB', '9'); // ID ИБ для видео
+define('IDPhotoIB', '5'); // ID ИБ для фото
+define('IDQuotesIB', '8'); // ID ИБ для блока цитат
+define('IDPartnersIB', '2'); // ID ИБ для блока с партнерами
+define('IDNewsIB', '6'); // ID ИБ для блока с новостями
+define('IDNominationIB', '7'); // ID ИБ для блока с описанием номинаций
+define('IDJuryIB', '4'); // ID ИБ для блока с информацией о жюри
+define('IDValuablePlayerIB', '3'); // ID ИБ для блока ценные игроки
+define('IDGentlemanYearIB', '11'); // ID ИБ для блока с лауреатами премии джентельмен года
+define('IDBestNewcomerIB', '12'); // ID ИБ для блока с лауреатами премии лучший новичок
+define('IDMostValuablePlayerIB', '10'); // ID ИБ для блока с лауреатами премии самый лучший игрок
 
 /*
  * Константы для обозначения ID Web-формы с заявкой
---
+++ local/php_interface/migrations/Version20230601120401.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20230601120401 extends Version
+{
+    protected $description = "Категории ИБ жюри";
+
+    protected $moduleVersion = "4.3.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+
+        $iblockId = $helper->Iblock()->getIblockIdIfExists(
+            'jury',
+            'people'
+        );
+
+        $helper->Iblock()->addSectionsFromTree(
+            $iblockId,
+            array (
+)        );
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
